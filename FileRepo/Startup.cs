cb0545a81b08df09c2cdd402c2a72438331fd258
sanjifly <sanjifly418@gmail.com>
2015-02-16 12:44:31 -0500

    AccountControl

---
+++ FileRepo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FileRepo.Startup))]
+namespace FileRepo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
