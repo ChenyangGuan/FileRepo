99fac5415051d43464e7a6a66355c42b950d0694
sanjifly <sanjifly418@gmail.com>
2015-02-22 11:15:39 -0500

    Newuserwillbenormal

---
+++ RoleBase/Controllers/DevelopersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoleBase.Controllers
+{
+    [Authorize(Roles = "Developer")]
+    public class DevelopersController : Controller
+    {
+        //
+        // GET: /Developers/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
