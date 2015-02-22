4c470a8f4dedfcfa2090e514dd6631cbaff8377b
sanjifly <sanjifly418@gmail.com>
2015-02-22 17:50:37 -0500

    Adduploadmultiplefiles

--- RoleBase/Controllers/DevelopersController.cs
+++ RoleBase/Controllers/DevelopersController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using RoleBase.Models;
+using System.IO;
 namespace RoleBase.Controllers
 {
     [Authorize(Roles = "Developer")]
@@ -14,6 +15,24 @@ namespace RoleBase.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        //
+        // POST: /Developers/
+        [HttpPost]
+        public ActionResult Index(Files f)
+        {
+            foreach (var file in f.files)
+            {
+                if (file.ContentLength > 0)
+                {
+                    var filename = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Uploads/"), filename);
+                    file.SaveAs(path);
+                }
+
+            }
+            return Content("Success");
         }
 	}
 }
